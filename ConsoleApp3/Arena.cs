e781108b58e218a239ba393b1a6f8311047f2010
tobiasengberg <tobias.engberg@exilic.se>
2024-07-01 11:25:10 +0200

    Full code

--- ConsoleApp3/Arena.cs
+++ ConsoleApp3/Arena.cs
@@ -2,5 +2,12 @@ namespace ConsoleApp3;
 
 public class Arena
 {
-    
+    public int Id { get; set; }
+    public string Place { get; set; }
+
+    public Arena(int id, string place)
+    {
+        Id = id;
+        Place = place;
+    }
 }
--- ConsoleApp3/QueryMethods.cs
+++ ConsoleApp3/QueryMethods.cs
@@ -2,5 +2,42 @@ namespace ConsoleApp3;
 
 public class QueryMethods
 {
+    public static string[] GetBasic(string[] words)
+    {
+        return words
+            .Where(w => w.Length > 5)
+            .Distinct()
+            .ToArray();
+    }
     
+    public static string[] GetOrdered(string[] words)
+    {
+        return words
+            .OrderByDescending(w => w)
+            .ToArray();
+    }
+    
+    public static string[] GetFewer(string[] words)
+    {
+        return words
+            .Take(10)
+            .OrderByDescending(w => w)
+            .ToArray();
+    }
+
+    public static string[] GetReverse(string[] words)
+    {
+        return words
+            .Take(10)
+            .Reverse()
+            .ToArray();
+    }
+    
+    public static string[] GetOdd(string[] words, string[] words2)
+    {
+        return words
+            .Except(words2)
+            .ToArray();
+    }
+
 }
